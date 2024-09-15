55543001da4c421023eaed58e84949ee75c7352b
Claudiu Traistaru <t.claudiu1990@gmail.com>
2024-09-15 14:41:38 +0300

    made app fully responsive

--- src/Components/Filters/Filters.tsx
+++ src/Components/Filters/Filters.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useReducer, useRef, useState } from "react";
 import './Filters.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 import { AppContext } from "../../App";
@@ -38,44 +38,58 @@ const Filters: React.FC<FiltersProps>=(props)=>{
         });
     }, [selectedDay, searchTitle, status])
 
+
+    const mobileFilters = useRef<HTMLDivElement>(null)
+    const toggleHideFilters = () => {
+        if(mobileFilters.current) {
+            mobileFilters.current.classList.contains('active') ? mobileFilters.current.classList.remove('active'):mobileFilters.current.classList.add('active')
+        }        
+    }
+    
     return(
         <div id="filters">
             <div className="filters-group">
+                <div>
+                    <button className="btn btn-new-task"
+                        onClick={()=>appContext?.addTask(
+                            {
+                                title: 'Task nou',
+                                description: 'Scrie aici descrierea taskului tau',
+                                startDate: selectedDay
+                            }
+                    )}>+ Creaza Task Nou</button>
+                    
+                    <label htmlFor="date-picker"> Data:</label>
+                    <DayPicker 
+                        selectedDay={selectedDay}
+                        onChange={handleCalendar}
+                    />
+                </div>
 
-                <button className="btn btn-new-task"
-                    onClick={()=>appContext?.addTask(
-                        {
-                            title: 'Task nou',
-                            description: 'Scrie aici descrierea taskului tau',
-                            startDate: selectedDay
-                        }
-                )}>+ Creaza Task Nou</button>
-                
-                <label htmlFor="date-picker"> Data:</label>
-                <DayPicker 
-                    selectedDay={selectedDay}
-                    onChange={handleCalendar}
-                />
+                <button className="btn-transparent btn-filters btn" onClick={toggleHideFilters}>Filtre &nbsp; <i className="fa-solid fa-filter"></i></button>
 
-                <button className="btn-transparent btn-filters btn">Filtre &nbsp; <i className="fa-solid fa-filter"></i></button>
 
                     
             </div>
 
             
-            <div className="filters-group mobile-filter-group">
+            <div className="filters-group mobile-filter-group" ref={mobileFilters}>
 
-                {/* <label htmlFor="search-input"><i className="fa-solid fa-magnifying-glass"></i></label> */}
-                <label htmlFor="search-input">Titlu</label>
-                <input id="search-input" type="text" onChange={(e)=>setSearchTitle(e.target.value)}></input>
+                <div>
+                    {/* <label htmlFor="search-input"><i className="fa-solid fa-magnifying-glass"></i></label> */}
+                    <label htmlFor="search-input">Titlu</label>
+                    <input id="search-input" type="text" onChange={(e)=>setSearchTitle(e.target.value)}></input>
+                </div>
 
-                <label htmlFor="select-status">Status</label>
-                <select name="status" id="select-status" onChange={(e)=>setStatus((e.target as HTMLSelectElement).value)}>
-                    <option value=''>Toate</option>
-                    <option value='create'>Creata</option>
-                    <option value='incurs'>In curs</option>
-                    <option value='finalizate'>Finalizata</option>
-                </select>
+                <div>
+                    <label htmlFor="select-status">Status</label>
+                    <select name="status" id="select-status" onChange={(e)=>setStatus((e.target as HTMLSelectElement).value)}>
+                        <option value=''>Toate</option>
+                        <option value='create'>Creata</option>
+                        <option value='incurs'>In curs</option>
+                        <option value='finalizate'>Finalizata</option>
+                    </select>
+                </div>
 
             </div>            
             
